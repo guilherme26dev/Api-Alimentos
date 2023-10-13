5bed36f4eb18963ac771188cd5c3a0da51adc296
Guilherme Henrique de Sousa Jesus <guilherme26hdev@hotmail.com>
2023-10-13 17:19:20 -0300

    ambiente inicial preparado

---
+++ api_food/src/main/java/org/devathome/api_food/ApiFoodApplication.java
@@ -0,0 +1,13 @@
+package org.devathome.api_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFoodApplication.class, args);
+	}
+
+}
---
+++ api_food/src/test/java/org/devathome/api_food/ApiFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devathome.api_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
