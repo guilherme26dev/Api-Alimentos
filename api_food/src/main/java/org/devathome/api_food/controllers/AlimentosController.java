95105a6b449a94b56bec902eed72b9b304a8b240
Guilherme Henrique de Sousa Jesus <guilherme26hdev@hotmail.com>
2023-10-28 02:49:03 -0300

    All ajusts done branches merge

--- api_food/src/main/java/org/devathome/api_food/controllers/AlimentosController.java
+++ api_food/src/main/java/org/devathome/api_food/controllers/AlimentosController.java
@@ -1,7 +1,10 @@
 package org.devathome.api_food.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.devathome.api_food.repository.AlimentosRepository;
+import org.devathome.api_food.services.AlimentosServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +15,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping(value="/api/v1/alimentos")
 public class AlimentosController {
+    private final AlimentosServices alimentosService;
+
     @Autowired
-    private AlimentosService alimentosService;
+    public AlimentosController(AlimentosServices alimentosService){
+        this.alimentosService = alimentosService;
+    }
 
     @GetMapping
-    public List<AlimentosService> getAll(){
-        return alimentosService.getAll();
+    public List<AlimentosRepository> getAllAlimentos(){
+        return alimentosService.findAll();
     }
     
     @GetMapping(value="{id}", produces = "application/json")
-    public AlimentosService getById(@RequestParam Long id) {
-        return alimentosSerice.findOne();
+    public Optional<AlimentosRepository> getById(@RequestParam Long id) {
+        return alimentosService.findOne(id);
     }
 
 
--- api_food/src/main/java/org/devathome/api_food/services/AlimentosServices.java
+++ api_food/src/main/java/org/devathome/api_food/services/AlimentosServices.java
@@ -1,17 +1,28 @@
 package org.devathome.api_food.services;
 
+import org.devathome.api_food.entity.AlimentosEntity;
+import org.devathome.api_food.repository.AlimentosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlimentosServices {
 
+    private final AlimentosRepository alimentosRepository;
+
     @Autowired
-    AlimentosRepository alimentosRepository;
-
-    List<AlimentosRepository> findAll(){
-
-        return repository.findAll();
+    public AlimentosServices(AlimentosRepository alimentosRepository) {
+        this.alimentosRepository = alimentosRepository;
     }
-    
+
+    public List<AlimentosRepository> findAll() {
+        return alimentosRepository.findAll();
+    }
+
+    public Optional<AlimentosRepository> findOne(Long id) {
+        return alimentosRepository.findById(id);
+    }
 }
